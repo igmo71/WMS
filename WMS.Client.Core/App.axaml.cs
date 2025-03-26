793dbd09ad90c0ba69e1a701e9e0ab85ecdd5fa6
shvechikov <shvechikovm@gmail.com>
2025-03-26 13:45:35 +0300

    Packages versioning fix

--- WMS.Client.Core/App.axaml.cs
+++ WMS.Client.Core/App.axaml.cs
@@ -1,7 +1,6 @@
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using WMS.Client.Core.ViewModels;
@@ -20,8 +19,6 @@ namespace WMS.Client.Core
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-                // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
                 DisableAvaloniaDataAnnotationValidation();
                 desktop.MainWindow = new MainWindow
                 {
@@ -41,11 +38,9 @@ namespace WMS.Client.Core
 
         private void DisableAvaloniaDataAnnotationValidation()
         {
-            // Get an array of plugins to remove
             var dataValidationPluginsToRemove =
                 BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
-            // remove each entry found
             foreach (var plugin in dataValidationPluginsToRemove)
             {
                 BindingPlugins.DataValidators.Remove(plugin);
--- WMS.Client.Core/ViewModels/MainViewModel.cs
+++ WMS.Client.Core/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace WMS.Client.Core.ViewModels
 {
