c41984979f14fdaeec6b60e988b24c9571e6c3cf
SHVED <shvechikovm@gmail.com>
2025-03-31 23:30:31 +0300

    Interactive

--- WMS.Client.Core/Behaviours/ClickBehaviour.cs
+++ WMS.Client.Core/Behaviours/ClickBehaviour.cs
@@ -8,17 +8,17 @@ namespace WMS.Client.Core.Behaviours
 {
     internal class ClickBehaviour : AvaloniaObject
     {
-        public static readonly AttachedProperty<ICommand> CommandProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Control, ICommand>("Command");
-        public static readonly AttachedProperty<object> CommandParameterProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Control, object>("CommandParameter");
+        public static readonly AttachedProperty<ICommand> CommandProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Interactive, ICommand>("Command");
+        public static readonly AttachedProperty<object> CommandParameterProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Interactive, object>("CommandParameter");
 
         public static void SetCommand(AvaloniaObject element, ICommand value) => element.SetValue(CommandProperty, value);
         public static ICommand GetCommand(AvaloniaObject element) => element.GetValue(CommandProperty);
         public static void SetCommandParameter(AvaloniaObject element, object value) => element.SetValue(CommandParameterProperty, value);
         public static object GetCommandParameter(AvaloniaObject element) => element.GetValue(CommandParameterProperty);
 
-        static ClickBehaviour() => CommandProperty.Changed.AddClassHandler<Control>(PropertyChangedHandler);
+        static ClickBehaviour() => CommandProperty.Changed.AddClassHandler<Interactive>(PropertyChangedHandler);
 
-        static void PropertyChangedHandler(Control element, AvaloniaPropertyChangedEventArgs args)
+        static void PropertyChangedHandler(Interactive element, AvaloniaPropertyChangedEventArgs args)
         {
             if (args.NewValue is ICommand commandValue)
                 element.AddHandler(InputElement.TappedEvent, EventHandler);
@@ -27,7 +27,7 @@ namespace WMS.Client.Core.Behaviours
         }
         static void EventHandler(object sender, RoutedEventArgs args)
         {
-            if (sender is Control control)
+            if (sender is Interactive control)
             {
                 ICommand command = control.GetValue(CommandProperty);
                 object parameter = control.GetValue(CommandParameterProperty);
