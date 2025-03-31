408d76e03de65dd8ee5dac34878a9812115cfbe3
SHVED <shvechikovm@gmail.com>
2025-03-31 23:33:52 +0300

    Name

--- WMS.Client.Core/Behaviours/TappedBehaviour.cs
+++ WMS.Client.Core/Behaviours/TappedBehaviour.cs
@@ -6,17 +6,17 @@ using System.Windows.Input;
 
 namespace WMS.Client.Core.Behaviours
 {
-    internal class ClickBehaviour : AvaloniaObject
+    internal class TappedBehaviour : AvaloniaObject
     {
-        public static readonly AttachedProperty<ICommand> CommandProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Interactive, ICommand>("Command");
-        public static readonly AttachedProperty<object> CommandParameterProperty = AvaloniaProperty.RegisterAttached<ClickBehaviour, Interactive, object>("CommandParameter");
+        public static readonly AttachedProperty<ICommand> CommandProperty = AvaloniaProperty.RegisterAttached<TappedBehaviour, Interactive, ICommand>("Command");
+        public static readonly AttachedProperty<object> CommandParameterProperty = AvaloniaProperty.RegisterAttached<TappedBehaviour, Interactive, object>("CommandParameter");
 
         public static void SetCommand(AvaloniaObject element, ICommand value) => element.SetValue(CommandProperty, value);
         public static ICommand GetCommand(AvaloniaObject element) => element.GetValue(CommandProperty);
         public static void SetCommandParameter(AvaloniaObject element, object value) => element.SetValue(CommandParameterProperty, value);
         public static object GetCommandParameter(AvaloniaObject element) => element.GetValue(CommandParameterProperty);
 
-        static ClickBehaviour() => CommandProperty.Changed.AddClassHandler<Interactive>(PropertyChangedHandler);
+        static TappedBehaviour() => CommandProperty.Changed.AddClassHandler<Interactive>(PropertyChangedHandler);
 
         static void PropertyChangedHandler(Interactive element, AvaloniaPropertyChangedEventArgs args)
         {
