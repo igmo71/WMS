32e38c45d7b9a8bcc0f063e7959a95dbee3484c6
SHVED <shvechikovm@gmail.com>
2025-04-02 13:32:18 +0300

    Adaptivity

--- WMS.Client.Core/Views/MainView.axaml.cs
+++ WMS.Client.Core/Views/MainView.axaml.cs
@@ -5,5 +5,13 @@ namespace WMS.Client.Core.Views
     public partial class MainView : UserControl
     {
         public MainView() => InitializeComponent();
+
+        private void SizeChanged(object? sender, SizeChangedEventArgs e)
+        {
+            bool portrait = e.NewSize.Height > e.NewSize.Width; 
+            CurrentName.IsVisible = portrait;
+            PagesButton.IsVisible = portrait;
+            PagesTabs.IsVisible = !portrait;
+        }
     }
 }
