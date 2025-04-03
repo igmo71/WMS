e87b2d486e606d31b31b1be773e47eee8e1d0ebc
Igor Mogilnitskiy <igmo71@outlook.com>
2025-04-03 14:47:41 +0300

    19  Create WMS.Shared and remove WMS.Shared.Abstractions.

---
+++ WMS.Shared/Models/Documents/Document.cs
@@ -0,0 +1,11 @@
+﻿namespace WMS.Shared.Models.Documents
+{
+    public abstract class Document
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Number { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
