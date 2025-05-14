0fa6bf272192afe3e8c871361f0ceb199e06d42d
Igor Mogilnitskiy <igmo71@outlook.com>
2025-05-14 20:00:24 +0300

    0046 Refactor AppDbContext. Recreate Migration Initialize.

--- WMS.Backend.Infrastructure/Data/AppDbContext.cs
+++ WMS.Backend.Infrastructure/Data/AppDbContext.cs
@@ -7,64 +7,77 @@ using WMS.Backend.Domain.Models.Documents;
 
 namespace WMS.Backend.Infrastructure.Data
 {
-    public class AppDbContext : IdentityDbContext<AppUser>
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<OrderIn> OrdersIn { get; set; }
-        public DbSet<OrderInProduct> OrderInProducts { get; set; }
-        public DbSet<OrderInArchive> OrdersInArchive { get; set; }
-
-        public DbSet<OrderOut> OrdersOut { get; set; }
-        public DbSet<OrderOutProduct> OrderOutProducts { get; set; }
+        #region Catalogs
 
         public DbSet<Product> Products { get; set; }
 
-        public DbSet<Warehouse> Warehouses { get; set; }
+        //public DbSet<Warehouse> Warehouses { get; set; }
+
+        #endregion
+
+        #region Documents
+
+        public DbSet<OrderIn> OrdersIn { get; set; }
+        public DbSet<OrderInArchive> OrdersInArchive { get; set; }
+        public DbSet<OrderInProduct> OrderInProducts { get; set; }
+
+        //public DbSet<OrderOut> OrdersOut { get; set; }
+        //public DbSet<OrderOutProduct> OrderOutProducts { get; set; }        
+
+        #endregion
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<OrderIn>().HasKey(e => e.Id);
-            modelBuilder.Entity<OrderIn>().Property(e => e.Number).HasMaxLength(AppSettings.NUMBER_MAX_LENGTH);
-            modelBuilder.Entity<OrderIn>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
-            modelBuilder.Entity<OrderIn>().Property(e => e.DateTime).HasConversion(
-                dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
-                dt => dt.ToLocalTime());
-            modelBuilder.Entity<OrderIn>().HasMany(e => e.Products).WithOne(e => e.Order)
-                .HasForeignKey(e => e.OrderId).HasPrincipalKey(e => e.Id)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<OrderInProduct>().HasKey(e => new { e.OrderId, e.ProductId });
-            modelBuilder.Entity<OrderInProduct>().HasOne(e => e.Product).WithMany()
-                .HasForeignKey(e => e.ProductId).HasPrincipalKey(e => e.Id)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<OrderInArchive>().HasKey(e => e.Id);
-            modelBuilder.Entity<OrderInArchive>().Property(e => e.DateTime).HasConversion(
-                dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
-                dt => dt.ToLocalTime());
-
-            modelBuilder.Entity<OrderOut>().HasKey(e => e.Id);
-            modelBuilder.Entity<OrderOut>().Property(e => e.Number).HasMaxLength(AppSettings.NUMBER_MAX_LENGTH);
-            modelBuilder.Entity<OrderOut>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
-            modelBuilder.Entity<OrderOut>().Property(e => e.DateTime).HasConversion(
-                dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
-                dt => dt.ToLocalTime());
-            modelBuilder.Entity<OrderOut>().HasMany(e => e.Products).WithOne()
-                .HasForeignKey(e => e.OrderId).HasPrincipalKey(e => e.Id);
-
-            modelBuilder.Entity<OrderOutProduct>().HasKey(e => new { e.OrderId, e.ProductId });
+            #region Catalogs
 
             modelBuilder.Entity<Product>().HasKey(e => e.Id);
             modelBuilder.Entity<Product>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
 
             modelBuilder.Entity<Warehouse>().HasKey(e => e.Id);
             modelBuilder.Entity<Warehouse>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
+
+            #endregion
+
+            #region Documents
+
+            modelBuilder.Entity<OrderIn>().HasKey(e => e.Id);
+            modelBuilder.Entity<OrderIn>().Property(e => e.Number).HasMaxLength(AppSettings.NUMBER_MAX_LENGTH);
+            modelBuilder.Entity<OrderIn>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
+            modelBuilder.Entity<OrderIn>().Property(e => e.DateTime)
+                .HasConversion(
+                    dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
+                    dt => dt.ToLocalTime());
+            modelBuilder.Entity<OrderIn>().HasMany(e => e.Products).WithOne(e => e.Order)
+                .HasForeignKey(e => e.OrderId).HasPrincipalKey(e => e.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<OrderInArchive>().HasKey(e => e.Id);
+            modelBuilder.Entity<OrderInArchive>().Property(e => e.DateTime)
+                .HasConversion(
+                    dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
+                    dt => dt.ToLocalTime());
+
+            modelBuilder.Entity<OrderInProduct>().HasKey(e => new { e.OrderId, e.ProductId });
+            modelBuilder.Entity<OrderInProduct>().HasOne(e => e.Product).WithMany()
+                .HasForeignKey(e => e.ProductId).HasPrincipalKey(e => e.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<OrderOut>().HasKey(e => e.Id);
+            //modelBuilder.Entity<OrderOut>().Property(e => e.Number).HasMaxLength(AppSettings.NUMBER_MAX_LENGTH);
+            //modelBuilder.Entity<OrderOut>().Property(e => e.Name).HasMaxLength(AppSettings.NAME_MAX_LENGTH);
+            //modelBuilder.Entity<OrderOut>().Property(e => e.DateTime).HasConversion(
+            //    dt => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
+            //    dt => dt.ToLocalTime());
+            //modelBuilder.Entity<OrderOut>().HasMany(e => e.Products).WithOne()
+            //    .HasForeignKey(e => e.OrderId).HasPrincipalKey(e => e.Id);
+
+            //modelBuilder.Entity<OrderOutProduct>().HasKey(e => new { e.OrderId, e.ProductId });
+            
+            #endregion
         }
     }
 }
