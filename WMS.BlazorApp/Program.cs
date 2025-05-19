d53fc5328f0663bf47be3a153207bed9a89e1d75
Igor Mogilnitskiy <igmo71@outlook.com>
2025-05-19 20:17:13 +0300

    0094 Fix Http clienr base address.

--- WMS.BlazorApp/Program.cs
+++ WMS.BlazorApp/Program.cs
@@ -11,8 +11,9 @@ namespace WMS.BlazorApp
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["BackendURL"]) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var backendURL = builder.Configuration["BackendURL"] ?? throw new InvalidOperationException("BackendURL not found");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(backendURL) });
 
             builder.Services.AddOidcAuthentication(options =>
             {
